630349e97ca4ccc98944d2506697d1b28a346fa5
mrraquarious <40706790+mrraquarious@users.noreply.github.com>
2023-04-07 23:03:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from pypdf import PdfReader
 import openai
 import streamlit as st
 
-from langchain.vectorstores import Pinecone
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.docstore.document import Document
+from langchain.vectorstores import FAISS, VectorStore
 
 from langchain import OpenAI
 from langchain.document_loaders import UnstructuredFileLoader
@@ -41,6 +44,36 @@ def parse_pdf(file: BytesIO) -> List[str]:
 
     return output
 
+def text_to_docs(text: str) -> List[Document]:
+    """Converts a string or list of strings to a list of Documents
+    with metadata."""
+    if isinstance(text, str):
+        # Take a single string as one page
+        text = [text]
+    page_docs = [Document(page_content=page) for page in text]
+
+    # Add page numbers as metadata
+    for i, doc in enumerate(page_docs):
+        doc.metadata["page"] = i + 1
+
+    # Split pages into chunks
+    doc_chunks = []
+
+    for doc in page_docs:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=800,
+            separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
+            chunk_overlap=0,
+        )
+        chunks = text_splitter.split_text(doc.page_content)
+        for i, chunk in enumerate(chunks):
+            doc = Document(
+                page_content=chunk, metadata={"page": doc.metadata["page"], "chunk": i}
+            )
+            # Add sources a metadata
+            doc.metadata["source"] = f"{doc.metadata['page']}-{doc.metadata['chunk']}"
+            doc_chunks.append(doc)
+    return doc_chunks
 
 def init_openai_settings():
     openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -162,15 +195,12 @@ def init_chat(chat_name):
         )
 
     if uploaded_file is not None:
-            texts = parse_pdf(uploaded_file)
+            docs = parse_pdf(uploaded_file)
+            texts = text_to_docs(docs)
+            st.write(text)
             with st.spinner("Indexing document... This may take a while⏳"):
                 embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key)
-                pinecone.init(
-                    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
-                    environment=PINECONE_API_ENV  
-                )
-                index_name = "rayai"
-                docsearch = Pinecone.from_texts([t for t in texts], embeddings, index_name=index_name)
+                docsearch = FAISS.from_documents(docs, embeddings)
     # with MAIN.container():
     answer_zoom = st.container()
     ask_form = st.empty()
