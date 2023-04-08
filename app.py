5aece3835d2424f5ad9fbcc6af94daf600cc002f
mrraquarious <40706790+mrraquarious@users.noreply.github.com>
2023-04-08 12:27:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.document_loaders import PyPDFLoader
 
 
-st.set_page_config(page_title="ChatGPT", page_icon🗃️")
+st.set_page_config(page_title="ChatGPT", page_icon="🗃️")
 
 MAIN = st.empty()
 
